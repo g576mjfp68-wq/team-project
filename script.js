300c487cc2fc75120ef5ac5ba401d8fd5ccaa038
Aurimas <g576mjfp68@privaterelay.appleid.com>
2026-09-10 19:46:20 +0300

    Pridėtas paspaudimų skaitiklis

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// Empty for now. Will be expanded later.
+const countButton = document.getElementById("count-button");
+const counterValue = document.getElementById("counter");
+
+let count = 0;
+
+countButton.addEventListener("click", () => {
+  count += 1;
+  counterValue.textContent = count;
+});
